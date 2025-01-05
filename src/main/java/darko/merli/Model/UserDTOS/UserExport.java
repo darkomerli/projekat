4303e72d1a3bc93b73a0d4f449d7389a35503efe
Darko Merli <darkomerli88@gmail.com>
2025-01-05 18:19:22 +0100

    added users created in the last hour cron

--- src/main/java/darko/merli/Model/UserDTOS/UserExport.java
+++ src/main/java/darko/merli/Model/UserDTOS/UserExport.java
@@ -3,8 +3,12 @@ package darko.merli.Model.UserDTOS;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 public class UserExport {
     private String userName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
