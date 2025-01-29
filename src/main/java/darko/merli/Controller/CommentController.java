6493020ee61c8735d784fc77b805a34d5d9e6199
Darko Merli <darkomerli88@gmail.com>
2025-01-29 15:22:04 +0100

    videoPage created

--- src/main/java/darko/merli/Controller/CommentController.java
+++ src/main/java/darko/merli/Controller/CommentController.java
@@ -6,10 +6,12 @@ import darko.merli.Service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @Validated
 @Tag(name = "3. Comments")
 //controller class which holds the apis regarding comments
@@ -19,14 +21,17 @@ public class CommentController {
     private CommentService commentService;
 
     @Operation(description = "Create a comment on selected video with ID", summary = "Create a comment")
-    @PostMapping("/videos/{id}/comment")
-    public CommentReturn createComment(@PathVariable int id, @RequestBody CommentCreate comment) {
-        return commentService.createComment(id, comment);
+    @PostMapping("/videos/{id}")
+    public String createComment(@PathVariable int id, @ModelAttribute("commentCreate") CommentCreate commentCreate) {
+        System.out.println(commentCreate.getContent());
+        commentService.createComment(id, commentCreate);
+        return "redirect:/videos/" + id;
     }
 
     @Operation(description = "Delete a comment with ID on selected video with ID", summary = "Delete the comment")
-    @DeleteMapping("/videos/{idVid}/comment/{idComm}")
+    @GetMapping("/videos/{idVid}/comment/{idComm}")
     public String deleteComment(@PathVariable long idVid, @PathVariable long idComm) throws IllegalAccessException {
-        return commentService.deleteComment(idVid,idComm);
+        commentService.deleteComment(idVid,idComm);
+        return "redirect:/videos/" + idVid;
     }
 }
